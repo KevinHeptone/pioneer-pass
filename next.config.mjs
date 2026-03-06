bf02281c81bc2bc20b8bfc531ff0318713794613
KevinHeptone <kevin@heptone.io>
2026-03-06 10:44:03 +0900

    Firebase 호스팅 설정 업데이트 및 Next.js 프로덕션 출력 설정 조정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  ...(process.env.NODE_ENV === "production" && {
+    output: "export",
+  }),
   images: {
     unoptimized: true,
   },
